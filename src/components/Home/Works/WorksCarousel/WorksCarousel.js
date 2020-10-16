76f91d4096b0e26f032ad312af12cb720bf47dad
HoqueTouhidul97 <67569444+HoqueTouhidul97@users.noreply.github.com>
2020-10-16 15:07:20 +0600

    done

--- src/components/Home/Works/WorksCarousel/WorksCarousel.js
+++ src/components/Home/Works/WorksCarousel/WorksCarousel.js
@@ -6,26 +6,27 @@ import carousel4 from '../../../../images/carousel-4.png';
 import carousel5 from '../../../../images/carousel-5.png';
 
 const WorksCarousel = () => {
+    
     return (
-        <div id="carouselExampleIndicators" className="carousel slide w-75 mx-auto pb-5" data-ride="carousel" data-interval="3000">
+        <div id="carouselExampleIndicators" className="carousel slide w-75 mx-auto pb-5" data-ride="carousel" data-interval="300">
             <ol className="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1" ></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2" ></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3" ></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4" ></li>
             </ol>
             <div className="carousel-inner pt-4 pb-5">
-                <div className="carousel-item active">
+                <div className="carousel-item active ">
                     <img className="d-block w-100" src={carousel1} alt="First slide"></img>
                 </div>
-                <div className="carousel-item">
+                <div className="carousel-item ">
                     <img className="d-block w-100" src={carousel2} alt="Second slide"></img>
                 </div>
-                <div className="carousel-item">
+                <div className="carousel-item ">
                     <img className="d-block w-100" src={carousel3} alt="Third slide"></img>
                 </div>
-                <div className="carousel-item">
+                <div className="carousel-item ">
                     <img className="d-block w-100" src={carousel4} alt="Fourth slide"></img>
                 </div>
                 <div className="carousel-item">
@@ -33,6 +34,7 @@ const WorksCarousel = () => {
                 </div>
             </div>
         </div>
+       
     );
 };
 
