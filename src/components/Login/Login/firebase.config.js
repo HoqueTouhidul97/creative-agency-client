0db034c83ad38b347b30c2fbbd0ee91516a77783
HoqueTouhidul97 <67569444+HoqueTouhidul97@users.noreply.github.com>
2020-10-16 10:31:39 +0600

    ready for deploy

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAU96-l3LnrWmark_B04ScCv61xLkoJ7i0",
+  authDomain: "creative-agency-23a90.firebaseapp.com",
+  databaseURL: "https://creative-agency-23a90.firebaseio.com",
+  projectId: "creative-agency-23a90",
+  storageBucket: "creative-agency-23a90.appspot.com",
+  messagingSenderId: "190511948256",
+  appId: "1:190511948256:web:f791b1f5c3c62072748b85"
+};
+
+export default firebaseConfig;
